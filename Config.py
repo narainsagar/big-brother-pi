14ec7c1efc938f22dbc023c05000a9e4e679948f
Abdulqadir <abdulqadir@Kashifs-Macbook.local>
2015-03-28 15:23:57 +0500

    minor fix

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 class Config(object):
     BASE_ADDR     = "http://localhost:9000"
-    NODE_ADDR     = BASE_ADDR + "/api/records/all"
-    LOG_ADDR      = BASE_ADDR + "/api/records/getlog"
+    NODE_ADDR     = BASE_ADDR + "/api/records/all/"
+    LOG_ADDR      = BASE_ADDR + "/api/records/getlog/"
     MAX_MSG_LEN   = ""
     SEND_DURATION = 3600
--- MessageDispatcher.py
+++ MessageDispatcher.py
@@ -12,7 +12,7 @@ class MessageDispatcher:
         c = pycurl.Curl()
         c.setopt(c.WRITEFUNCTION, buf.write)
         c.setopt(c.URL, curlMsg.getPath())
-        c.setopt(c.POSTFIELDS, 'data='+curlMsg.getBody())
+        c.setopt(c.HTTPPOST, [('data',curlMsg.getBody())])
         #c.setopt(c.VERBOSE, True)
         c.setopt(c.FAILONERROR, True)
 
@@ -23,16 +23,3 @@ class MessageDispatcher:
         finally:
             print buf.getvalue() #TODO: log value in buffer
             c.close()
-
-    def dispatchMulti(self, curlMsgList):
-        buf = cStringIO.StringIO()
-        mc = pycurl.CurlMulti()
-        for msg in curlMsgList:
-            c = pycurl.Curl()
-            c.setopt(c.WRITEFUNCTION, buf.write)
-            c.setopt(c.URL, msg.getPath())
-            c.setopt(c.POSTFIELDS, 'data='+msg.getBody())
-            c.setopt(c.FAILONERROR, True)
-            mc.add_handle(c)
-
-        #TODO: implement rest of the function
