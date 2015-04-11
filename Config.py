fc371ac4c1560d9f0b6d906677113ba3e8110025
Suleman <suleman@marketlytics.com>
2015-04-11 11:16:22 +0500

    Linked with deployed server and changed routes

--- Config.py
+++ Config.py
@@ -1,6 +1,7 @@
 class Config(object):
-    BASE_ADDR     = "http://localhost:8888"
-    NODE_ADDR     = BASE_ADDR + "/pistuff/"
-    LOG_ADDR      = BASE_ADDR + "/pistuff/"
+    BASE_ADDR     = "http://big-brother.herokuapp.com/api/records"
+    NODE_ADDR     = BASE_ADDR + "/all"
+    LOG_ADDR      = BASE_ADDR + "/getlog"
+    COMPANY_ID = ""
     MAX_MSG_LEN   = ""
     SEND_DURATION = 3600
